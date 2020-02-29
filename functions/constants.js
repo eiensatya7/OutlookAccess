2a98d419ea12ba24fa9cfee2f93e95ac4343a20e
Pallu <pallu@pallus-mbp.home>
2020-02-29 04:11:44 +0000

    feature: Support to check events for tomorrow

--- functions/constants.js
+++ functions/constants.js
@@ -1,10 +1,16 @@
 const messages = {
     SIGNED_IN_MESSAGE: 'Hello! How may I help you?',
     SIGNIN_FAILED_MESSAGE: 'Hm. There was something wrong while you tried to sign in',
-    CHECKING_EVENTS: 'Ok let me check your events for today',
+    CHECKING_EVENTS: 'Ok let me check your events',
 };
+
+const days = {
+    TODAY: 'today',
+    TOMORROW: 'tomorrow'
+}
+
 const api = {
     GET_CALENDAR_VIEW: `https://graph.microsoft.com/v1.0/me/calendarview?StartDateTime=$1&EndDateTime=$2`,
 };
 
-module.exports = {messages, api};
+module.exports = {messages, api, days};
--- functions/outlookMeetings.js
+++ functions/outlookMeetings.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch');
 const moment = require('moment');
-const {api} = require('./constants');
+const {api, days} = require('./constants');
 const utils = require('./utils');
 const {Logging} = require('@google-cloud/logging');
 const logging = new Logging();
@@ -9,43 +9,58 @@ const logging = new Logging();
 /**
  * Outlook Integration module
  */
-class OutlookMeetings {
-    getNumberOfMeetings(meetings) {
-        return `You have ${meetings.length} ${meetings.length === 1 ? 'meeting' : 'meetings'} today`;
-    }
 
-    getOrganizerName(meeting) {
-        return meeting.organizer.emailAddress.name || 'Name not available';
-    }
+const getNumberOfMeetings = (meetings, preferredDay) => `You have ${meetings.length} ${meetings.length === 1 ? 'meeting' : 'meetings'} ${preferredDay}.`;
 
-    getMoreInfoAboutMeetings(meetings) {
-        if (meetings.length === 1) {
-            return `with subject ${meetings[0].subject} with ${this.getOrganizerName(meetings[0])}`;
-        }
-        return meetings.map((meeting, index) => {
-            return `Meeting ${index+1} - with subject ${meeting.subject} with ${this.getOrganizerName(meeting)}`;
+const getOrganizerName = (meeting) => meeting.organizer.emailAddress.name || 'Name not available.';
+
+const getMoreInfoAboutMeetings = (meetings) => {
+    if (meetings.length === 1) {
+        return `with subject ${meetings[0].subject} with ${getOrganizerName(meetings[0])}.`;
+    }
+    return meetings.map((meeting, index) => {
+        return `Meeting ${index+1} - with subject ${meeting.subject} with ${getOrganizerName(meeting)}.`;
+    });
+};
+
+const formatResponse = ({ value }, preferredDay) => {
+    const meetings = value;
+    if (!meetings || meetings.length === 0) {
+        return `You have no meetings ${preferredDay}!`;
+    }
+    return `${getNumberOfMeetings(meetings, preferredDay)} ${getMoreInfoAboutMeetings(meetings)}`;
+};
+
+
+
+const getStartAndEndOfDay = preferredDay => {
+    if(preferredDay === days.TOMORROW) {
+        return ({
+            startDate: moment().add(1, 'day').startOf('day').format(moment.HTML5_FMT.DATETIME_LOCAL_SECONDS),
+            endDate: moment().add(1, 'day').endOf('day').format(moment.HTML5_FMT.DATETIME_LOCAL_SECONDS),
         });
     }
+    return ({
+        startDate: moment().startOf('day').format(moment.HTML5_FMT.DATETIME_LOCAL_SECONDS),
+        endDate: moment().endOf('day').format(moment.HTML5_FMT.DATETIME_LOCAL_SECONDS),
+    });
+};
 
-    formatResponseForToday({ value }) {
-        const meetings = value;
-        if (!meetings || meetings.length === 0) {
-            return 'You have no meetings today!';
-        }
-        return `${this.getNumberOfMeetings(meetings)} ${this.getMoreInfoAboutMeetings(meetings)}`;
-    }
+    // Below function will get the events for specific day and it will defaults to current day.
+const getEventsForSpecificDay = (conv, params, accessToken) => {
+    console.log('date-time: ', params['date-time']);
+    const preferredDay = params['date-time'] || days.TODAY;
+    const { startDate, endDate } = getStartAndEndOfDay(preferredDay);
+    const url = utils.parameterize(api.GET_CALENDAR_VIEW, startDate, endDate);
+    console.log('url is ' + url + ' this crap is ' + api.GET_CALENDAR_VIEW);
+    return fetch(url, {
+        headers: {'Content-Type': 'application/json', Authorization: `Bearer ${accessToken}`},
+    }).then((res) => res.json())
+        .then((json) => conv.ask(formatResponse(json, preferredDay)))
+        .catch((error) => console.error('error:', error));
+};
 
-    getEventsForToday(conv, accessToken) {
-        const startDate = moment().startOf('day').format(moment.HTML5_FMT.DATETIME_LOCAL_SECONDS);
-        const endDate = moment().endOf('day').format(moment.HTML5_FMT.DATETIME_LOCAL_SECONDS);
-        const url = utils.parameterize(api.GET_CALENDAR_VIEW, startDate, endDate);
-        console.log('url is ' + url + ' this crap is ' + api.GET_CALENDAR_VIEW);
-        return fetch(url, {
-            headers: {'Content-Type': 'application/json', Authorization: `Bearer ${accessToken}`},
-        }).then((res) => res.json())
-            .then((json) => conv.close(this.formatResponseForToday(json)))
-            .catch((error) => console.error('error:', error));
-    }
-}
 
-module.exports = OutlookMeetings;
+module.exports = {
+    getEventsForSpecificDay,
+};
